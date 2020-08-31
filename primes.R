a9a576b9688fc31b97076bc14690fff87d198162
Jonathan Schwartz <jzs1986@gmail.com>
2020-08-31 11:41:25 -0700

    Update primes.R

--- primes.R
+++ primes.R
@@ -1,10 +1,9 @@
-library(tictoc)
+library(beepr)
 
 primes = read.csv("primes.txt",header=F) #source: http://www.naturalnumbers.org/primes.html
 last_digits = primes[,2]%%10
 
-
-tic()
+#this for loop stores each frame of the animation as a png that I later turn into a gif using Gimp
 for(i in 1:1000){
   s = paste("thousands",i,".png",sep="")
   png(filename = s)
@@ -15,8 +14,9 @@ for(i in 1:1000){
        xlab="Final Digit")
   dev.off()
 }
-toc()
+beep()
 
+#this for loop shows the same thing but it displays it right in your R console
 for(i in 1:1000){
   plot(table(last_digits[1:i*1000]),
        main=paste("Final digits of first ",i*1000, " primes",
@@ -24,22 +24,4 @@ for(i in 1:1000){
        ylab="Count",
        xlab="Final Digit")
   Sys.sleep(.1)
-  #dev.off()
-}
-
-
-first_digit <-function(n){
-  return(as.numeric(substr(as.character(n),1,1)))
-}
-
-firsts = first_digit(primes[,2])
-
-for(i in 1:1000){
-  plot(table(firsts[1:i*1000]),
-       main=paste("Final digits of first ",i*1000, " primes",
-                  "\n",i*1000,"th prime: ",primes[i*1000,2],sep=""),
-       ylab="Count",
-       xlab="Final Digit")
-  Sys.sleep(.1)
-  #dev.off()
 }
