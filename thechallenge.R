29ac096975a67c837401694fa0fdbe0e3bd92b49
Jonathan Schwartz <jzs1986@gmail.com>
2020-08-31 11:05:04 -0700

    Update thechallenge.R

--- thechallenge.R
+++ thechallenge.R
@@ -1,3 +1,9 @@
+#The Challenge is a show that has run on MTV for 35 seasons. Orginally they took cast members from the Real World
+#and Road Rules, but over the years it has opened up to many other shows they pull contestants from.
+#This visualization shows the progression of which cast members appeared on which seasons, in addition to
+#which show they originally appeared on (Real World, Road Rules, etc.), and what their result was on that particular 
+#season of the challenge (winner, finalist, etc.).
+
 library(readxl)
 library(RColorBrewer)
 library(beepr)
